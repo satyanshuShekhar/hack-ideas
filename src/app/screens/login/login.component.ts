ba03c45628f53e982bd07f50cecbce01fc0013ae
Satyanshu Shekhar <satyanshushekhar@virtualemployee.com>
2022-05-19 20:29:38 +0530

    Login UI done

--- src/app/screens/login/login.component.ts
+++ src/app/screens/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) {
+    this.initLoginForm();
   }
 
+  ngOnInit(): void {}
+
+  /**
+   * Initialise the login form.
+   */
+  private initLoginForm() {
+    this.loginForm = this.formBuilder.group({
+      employeeId: new FormControl(
+        '',
+        Validators.compose([Validators.required])
+      ),
+      rememberMe: new FormControl(false),
+    });
+  }
+
+  login() {
+    if (this.loginForm.invalid) {
+      console.log('Form Invalid');
+    } else {
+      console.log('Login success');
+    }
+  }
 }
